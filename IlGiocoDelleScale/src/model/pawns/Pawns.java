73bd68b9c702cc736fa5406b66451f085e4ed573
Thomas <thomas.angelini@studio.unibo.it>
2018-05-11 18:05:42 +0200

    Position is a Pair type

--- IlGiocoDelleScale/src/model/pawns/Pawns.java
+++ IlGiocoDelleScale/src/model/pawns/Pawns.java
@@ -1,14 +1,15 @@
 package model.pawns;
 
 import enumeration.Characters;
+import utilities.Pair;
 
 public interface Pawns {
 
-	public int getPosition();
+	public Pair<Integer,Integer> getPosition();
 	
-	public void setPosition(int pos);
+	public void setPosition(Pair<Integer,Integer> pos);
 
-	public boolean checkPossible(int pos);
+	public boolean checkPossible(Pair<Integer, Integer> pos);
 	
 	public void setPawn(Characters car);
 	
