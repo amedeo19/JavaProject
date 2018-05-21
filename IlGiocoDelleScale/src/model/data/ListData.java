ecf06fa96bf7de320f0267e564bee16febc430d6
Thomas <thomas.angelini@studio.unibo.it>
2018-05-21 21:39:42 +0200

    Interface factory for Data

---
+++ IlGiocoDelleScale/src/model/data/ListData.java
@@ -0,0 +1,14 @@
+package model.data;
+
+import model.dice.*;
+import java.util.List;
+
+public interface ListData {
+
+	Data classicMode();
+	
+	Data personalizedMode(int cellNumber,List<Dice> diceList);
+	
+	public void checkList();
+	
+}
