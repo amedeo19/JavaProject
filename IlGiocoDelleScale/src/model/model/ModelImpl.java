71076ab7348a5507daf0eac1f2eae791661c3585
Thomas <thomas.angelini@studio.unibo.it>
2018-05-22 09:56:06 +0200

    Implements of Model with pattern singleton

--- IlGiocoDelleScale/src/model/model/ModelImpl.java
+++ IlGiocoDelleScale/src/model/model/ModelImpl.java
@@ -1,26 +1,22 @@
 package model.model;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 
-import model.dice.Dice;
+import model.data.Data;
 import model.movements.Movements;
+import model.movements.MovementsImpl;
 import model.pawns.Pawns;
 
 public class ModelImpl implements Model{
 
 	Movements move;
-	Optional<List<Dice>> diceList;
-	Optional<Integer> lastNumber;
 	boolean done;
-	private static final int MAXDICE=3;
-	private static final Supplier<RuntimeException> buildError = () -> new IllegalStateException("Build Error");
-	private static final Supplier<RuntimeException> diceError = () -> new IllegalStateException("Dice Error");
+	private static final Supplier<RuntimeException> initError = () -> new IllegalStateException("Init Error");
 	
 	
-	public ModelImpl() {
-		this.reset(); 
+	public ModelImpl() { 
+		this.move=MovementsImpl.getLog();
 	}
 	
 
@@ -31,73 +27,50 @@ public class ModelImpl implements Model{
 	public static ModelImpl getLog() {
 		return LazyHolder.SINGLETON;
 	}
-	
 
 	@Override
-	public void setDice(List<Dice> dicelist) {
-
-		this.isDone(false);
-		this.diceList=Optional.ofNullable(dicelist);
-	}
-
-	@Override
-	public List<Dice> getDice() {
+	public synchronized int movePawn(Pawns p) {
 		
-		this.isDone(true);
-		return this.diceList.get();
-	}
-
-	@Override
-	public void build() {
-		
-		this.isDone(false);
-		if (!this.diceList.isPresent() || this.diceList.get().size()>MAXDICE){
-			throw diceError.get();
-		}
-		if (!this.lastNumber.isPresent()){
-			throw new IllegalStateException("Error with number of cell");
-		}
-		this.done=true;
-	}
-
-	@Override
-	public void reset() {
-		
-		this.diceList=Optional.empty();
-		this.lastNumber=Optional.empty();
-		this.done=false;
-	}
-
-	@Override
-	public void setFinish(int finish) {// control if >0 and if already built
-		if (finish<=0){
-			throw new IllegalStateException("Unreal finish");
-		}
-		this.lastNumber=Optional.of(finish);
-	}
-
-	@Override
-	public int getFinishNumber() { 
-		this.isDone(true);
-		return this.lastNumber.get();
-	}
-
-	
-	@Override
-	public void isDone(boolean value) {// va bene col true?
-		if (this.done!=value){
-			throw buildError.get();
-		}
-	}
-
-
-	@Override
-	public int movePawn(Pawns p) {
-		this.isDone(true);
-		this.move.changePosition(p, this.diceList.get(),this.lastNumber.get());
+		this.check();
+		this.move.changePosition(p);
 		p.setState(!p.getState());
+		System.out.println(p.getPosition());
 		return p.getPosition();
 	}
+
+	@Override
+	public void startGame(Data data) {
+		
+		this.move.setData(data);
+		this.changeDone();
+	}
+
+	@Override
+	public void endGame() {
+		
+		this.changeDone();
+		this.move.reset();
+	}
+
+	@Override
+	public void check() {
+		if (!this.done){
+			throw initError.get();
+		}
+	}
+
+	@Override
+	public void changeDone() {
+		this.done=!this.done;
+	}
+
+	@Override
+	public synchronized boolean checkWin(Pawns p) {
+		
+		this.check();
+		return this.move.checkWin(p.getPosition());
+	}
+	
 	
 
 
