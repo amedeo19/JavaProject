319538c3ae8bfbd78b32f560560494b808d6540c
Thomas <thomas.angelini@studio.unibo.it>
2018-05-22 09:49:55 +0200

    Interface for dice movement and check possible victory

---
+++ IlGiocoDelleScale/src/model/movements/Movements.java
@@ -0,0 +1,20 @@
+package model.movements;
+
+import model.data.Data;
+import model.pawns.Pawns;
+
+public interface Movements {
+
+	public void changePosition(Pawns p);
+	
+	public void setData(Data data);
+	
+	public Data getData();
+	
+	public void done(boolean value);
+	
+	public void reset();
+	
+	public boolean checkWin(Pawns p);
+	
+}
