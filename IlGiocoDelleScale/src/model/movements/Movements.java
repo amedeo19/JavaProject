67d8e2c7ffe850b7addfdc6344299fdc9350607a
Thomas <thomas.angelini@studio.unibo.it>
2018-07-26 15:22:36 +0200

    Movement build

--- IlGiocoDelleScale/src/model/movements/Movements.java
+++ IlGiocoDelleScale/src/model/movements/Movements.java
@@ -7,14 +7,17 @@ public interface Movements {
 
 	public void changePosition(Pawns p);
 	
+	public int dicePosition(int initialPosition, int diceNumber);
+	
 	public void setData(Data data);
 	
 	public Data getData();
 	
-	public void done(boolean value);
-	
 	public void reset();
 	
 	public boolean checkWin(int pos);
+	
+	public void build();
+	
 	
 }
