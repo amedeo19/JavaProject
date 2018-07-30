2e3414e79532660ba4291cecf40261366869f62a
Amedeo Bertuccioli <amedeo.bertuccioli@studio.unibo.it>
2018-07-30 16:02:27 +0200

    Added method

--- IlGiocoDelleScale/src/controller/Controller.java
+++ IlGiocoDelleScale/src/controller/Controller.java
@@ -1,14 +1,19 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
+
+import model.dice.Dice;
 
 public interface Controller {
 
 	
-	public void play();
+	public void rollDice();
 	
 	public void start();
 	
 	public void FinishGame() throws IOException;
+	
+	public void Play(int NumPlayers, List<Dice> DiceList);
 
 }
--- IlGiocoDelleScale/src/controller/ControllerImpl.java
+++ IlGiocoDelleScale/src/controller/ControllerImpl.java
@@ -4,6 +4,7 @@ package controller;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import model.data.Data;
 import model.data.DataImpl;
@@ -19,27 +20,28 @@ public class ControllerImpl implements Controller {
 
 	private static final ControllerImpl SINGLETON = new ControllerImpl();
 	private final Model game;
-	
 	private int turn;
 	private boolean control;	//dadi
 	List<Dice> diceList;
+	List<Pawns> PawnsList;			//per ogni pedone occorre aggiungere un numero identificativo per gestire il turno
 	int lastNumber;
 	private final Data data;
 	private final ListData list;
 	private final Pawns p;
+	private Optional<Settings> setting;
 	
 	
 	
-	public ControllerImpl(List<Dice> list, int lastNumber) {
+	public ControllerImpl(List<Dice> list, int lastNumber, List<Pawns> Pawns) {
 		this.game = new ModelImpl();
 		this.turn = 0;
 		this.diceList = list;
+		this.PawnsList = Pawns;
 		this.lastNumber = lastNumber;
-		
+		this.setting = Optional.empty();
 		this.list = new ListDataImpl();
 		this.list.personalizedMode(lastNumber, diceList);
 		this.data = new DataImpl(this.diceList, this.lastNumber);
-		
 		this.p = new PawnsImpl();
 	}
 
@@ -47,6 +49,7 @@ public class ControllerImpl implements Controller {
 	public ControllerImpl() {
 		this.game = new ModelImpl();
 		this.turn = 0;
+		this.setting = Optional.empty();
 		this.list = new ListDataImpl();
 		this.diceList.add(new ListDiceImpl().classicDice());
 		this.data = new DataImpl(this.diceList, 100);
@@ -59,16 +62,26 @@ public class ControllerImpl implements Controller {
     }
 	
 	
-	
-	public void play() {
+	public void rollDice() {
         if(control) {
 			this.game.getLog();
 	        this.game.startGame(data);
-	        int a = this.game.movePawn(p);
-	        this.p.setPosition(a);
-	        this.p.setState(false);
-	        if(this.game.checkWin(p)) {
-	        	this.game.endGame();
+	        Pawns k = this.PawnsList.get(turn);
+	        int a = this.game.movePawn(k);
+	        int b = this.data.getFinishNumber();		//cosi prendo il risultato dell'ultimo lancio di quel dado
+	        k.setPosition(a);
+	        k.setState(false);
+	        if (this.turn < this.setting.get().getNumPlayers() - 1) {
+	        	this.turn++;
+	        } else {
+	        	this.turn=0;
+	        }
+	        if(this.game.checkWin(k)) {
+	        	try {
+					this.FinishGame();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 	        }
         } else {
         	throw new IllegalStateException();
@@ -89,6 +102,22 @@ public class ControllerImpl implements Controller {
 	
 	public void start() {
 		this.control = true;
+	}
+
+
+	@Override
+	public void Play (int NumPlayers, List<Dice> DiceList) {
+		if(this.control) {
+			this.setting = Optional.of(new SettingBuilder()
+					.NumPlayers(NumPlayers)
+					.Dice(DiceList)
+					.build());
+			
+			this.turn = 0;
+			
+		} else {
+			throw new IllegalStateException();
+		}
 	}
 	
 	
