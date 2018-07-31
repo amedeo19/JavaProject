2df2062363aaf9042c4fc970ca8fb1f398107b95
Thomas <thomas.angelini@studio.unibo.it>
2018-07-31 11:08:01 +0200

    Not Builder

--- IlGiocoDelleScale/src/model/data/DataImpl.java
+++ IlGiocoDelleScale/src/model/data/DataImpl.java
@@ -1,96 +1,41 @@
 package model.data;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Supplier;
 import model.dice.Dice;
 
 public class DataImpl implements Data{
 
-	Optional<List<Dice>> diceList;
-	Optional<Integer> lastNumber;
-	boolean done;
+	List<Dice> diceList;
+	int lastNumber;
 	private final static int MINFINISH=30;
 	private final static int MAXFINISH=200;
 	private static final int MAXDICE=3;
-	private static final Supplier<RuntimeException> buildError = () -> new IllegalStateException("Build Error");
-	private static final Supplier<RuntimeException> diceError = () -> new IllegalStateException("Dice Error");
+	private static final Supplier<RuntimeException> diceError = () -> new IllegalStateException("Too many Dice");
 	private static final Supplier<RuntimeException> cellError = () -> new IllegalStateException("Error with number of cell");
-	private static final Supplier<RuntimeException> dataError = () -> new IllegalStateException("Not already built");
-	
-	private static final Data SINGLETON = new DataImpl();
-	
-	public static Data getData() {
-        return SINGLETON;
-	}
-	
-	private void isDone() {								
+
+	public DataImpl(List<Dice> diceList,int lastNumber) {
 		
-		if (!this.done){
-			throw buildError.get();
-		}
-	}
-	
-	private void checkNotDone() {								
-		
-		if (this.done){
-			throw dataError.get();
-		}
-	}
-	
-	@Override
-	public void setDice(List<Dice> listDice) {
-		
-		this.checkNotDone();
-		if ((listDice.isEmpty()) || (listDice.size())>MAXDICE){
+		this.diceList=diceList;
+		if (this.diceList.size()>MAXDICE){
 			throw diceError.get();
 		} 
-		this.diceList=Optional.of(listDice);
-	}
-
-	@Override
-	public void build() {
-		
-		this.checkNotDone();
-		if (!this.diceList.isPresent()){
-			throw diceError.get();
-		}
-		if (!this.lastNumber.isPresent()){
+		this.lastNumber=lastNumber;
+		if ((this.lastNumber<MINFINISH) || (this.lastNumber>MAXFINISH)){
 			throw cellError.get();
 		}
-		this.done=true;
 	}
-
-	@Override
-	public void reset() {
-		
-		this.diceList=Optional.empty();
-		this.lastNumber=Optional.empty();
-		this.done=false;
-	}
-
-	@Override
-	public void setFinish(int finish) {// control if >0 and if already built
-		
-		this.checkNotDone();
-		if ((finish<MINFINISH) || (finish>MAXFINISH)){
-			throw cellError.get();
-		}
-		this.lastNumber=Optional.of(finish);
-	}	
 	
 	@Override
 	public int getFinishNumber() {
 		
-		this.isDone();
-		return this.lastNumber.get();
+		return this.lastNumber;
 	}
 
 	@Override
 	public List<Dice> getDice() {
 		
-		this.isDone();
-		return this.diceList.get();
+		return this.diceList;
 	}
 	
 	
