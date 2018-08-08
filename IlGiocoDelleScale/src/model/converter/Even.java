57acefef9104719fead8a01d63216c51b7b90625
Thomas <thomas.angelini@studio.unibo.it>
2018-08-08 12:28:23 +0200

    Nothing Cahnged

--- IlGiocoDelleScale/src/model/converter/Even.java
+++ IlGiocoDelleScale/src/model/converter/Even.java
@@ -2,10 +2,11 @@ package model.converter;
 
 import model.board.Coordinate;
 
-public class Even implements Strategy{
+public class Even implements ConverterStrategy{
 
 	@Override
 	public int getNumber(Coordinate coordinate) {
+	
 		return ((coordinate.getY()*10)+(coordinate.getX()));
 	}
 
