b352ed4a3360d7622ff4cd41a5eaad5caa59b1d7
Thomas <thomas.angelini@studio.unibo.it>
2018-08-09 11:36:56 +0200

    Class for convert even height

--- IlGiocoDelleScale/src/model/converter/Even.java
+++ IlGiocoDelleScale/src/model/converter/Even.java
@@ -2,12 +2,24 @@ package model.converter;
 
 import model.board.Coordinate;
 
-public class Even implements ConverterStrategy{
+public class Even extends AbstractConverter{
 
+	public Even(int width) {
+		
+		super(width);
+	}
+	
 	@Override
 	public int getNumber(Coordinate coordinate) {
 	
-		return ((coordinate.getY()*10)+(coordinate.getX()));
+		return ((coordinate.getY()*super.getWidth())+((coordinate.getX())+1));
 	}
 
+	@Override
+	public Coordinate getCoordinate(int num) {
+		
+		return new Coordinate((num -(this.getHeight(num)*super.getWidth())+1), (this.getHeight(num)));
+	}
+
+	
 }
