353cc11bb6fcc8265235bf177a78b7e070cde32f
Thomas <thomas.angelini@studio.unibo.it>
2018-08-09 11:34:37 +0200

    Converter class

--- IlGiocoDelleScale/src/model/converter/ConverterImpl.java
+++ IlGiocoDelleScale/src/model/converter/ConverterImpl.java
@@ -11,23 +11,43 @@ public class ConverterImpl implements Converter{
 	public ConverterImpl(int width) {
 		
 		this.width=width;
-		this.Even=new Even();
+		this.Even=new Even(this.width);
 		this.Odd=new Odd(this.width);
 	}
 	
 	@Override
-	public int convert(Coordinate coordinate) {
+	public int toInt(Coordinate coordinate) {
 		
-		if (this.checkEven(coordinate.getY())){
+		if (this.checkIntEven(coordinate.getY())){
 			return this.Even.getNumber(coordinate);
 		}else{
 			return this.Odd.getNumber(coordinate);
 		}
 	}
 	
-	private boolean checkEven(int num){
+	@Override
+	public Coordinate toCoordinate(int num) {
+		
+		if (this.checkCooEven(this.getHeight(num))){
+			return this.Even.getCoordinate(num);
+		}else{
+			return this.Odd.getCoordinate(num);
+		}
+		
+	}
+	
+	private boolean checkIntEven(int num){
 		
 		return (num%2)==0;
 	}
-
+	
+	private int getHeight(int num){
+		
+		return (num/this.width);
+	}
+	
+	private boolean checkCooEven(int num){
+		
+		return (num%2)==0;
+	}
 }
