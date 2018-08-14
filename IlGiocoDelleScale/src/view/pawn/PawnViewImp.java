261fcd301d697754157f410c263b2a1e6694e9e5
Amedeo Bertuccioli <amedeo.bertuccioli@studio.unibo.it>
2018-08-14 19:07:08 +0200

    Modified view Pawn method

--- IlGiocoDelleScale/src/view/pawn/PawnViewImp.java
+++ IlGiocoDelleScale/src/view/pawn/PawnViewImp.java
@@ -1,76 +1,44 @@
 package view.pawn;
 
 
-import enumeration.Characters;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.awt.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-import javafx.scene.control.Tooltip;
-import javafx.scene.image.Image;
+import enumeration.Characters;
+import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
-
 import model.pawns.Pawns;
-import model.model.ModelImpl;
-import enumeration.Characters;
+import model.pawns.PawnsImpl;
+import model.board.Coordinate;
 
 
-public class PawnViewImpl extends ImageView implements PawnView {
+public class PawnViewImp implements PawnView {
 
-	public static final int STANDARD_DIM = 20;
-	private static final double SPACE_BOX = 2.5;
-	private final Characters character;
-	private final Pawns pawn;
-	private static final int START = 0;
-	private static int pos;
-	
-	
-	public PawnViewImpl(final Characters character, final Pawns p) {
-		super();
-		this.pawn = p;
-		this.pos = START;
-		this.character = character;
-		Tooltip.install(this, new Tooltip(character.toString()));
-		this.setPreserveRatio(true);
-		this.setFitHeight(STANDARD_DIM);
-		switch(this.getPawn()) {
-		case Baloo:
-			this.setImage(new Image(this.getClass().getResourceAsStream("/Pawns/Balooo.png")));
-			break;
-		case Baghera:
-			this.setImage(new Image(this.getClass().getResourceAsStream("/Pawns/BagheraLaPanteraNera.png")));
-			break;
-		case ShereKhan:
-			this.setImage(new Image(this.getClass().getResourceAsStream("/Pawns/shereKhan.png")));
-			break;
-		case KingLouie:
-			this.setImage(new Image(this.getClass().getResourceAsStream("/Pawns/ReLuigi.png")));
-			break;
-		default:
-			this.setImage(new Image(this.getClass().getResourceAsStream("/Pawns/BagheraLaPanteraNera.png")));
-			break;
-		}
+	@FXML
+	private Label viewPawn;
+	@FXML
+	private ImageView image;
+	private Pawns pawn;
+	private Characters character;
+	private Coordinate coordinate;
+
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		this.viewPawn.setLocation(this.coordinate.getX(), this.coordinate.getY());
+		this.pawn = new PawnsImpl();
+		this.viewPawn.setVisible(true);
 		
 	}
-	
-	
-	@Override
-	public Characters getPawn() {
-		return this.character;
-	}
-	
-	
-	public void SetInitPos() {
-		this.pos = START;
-	}
 
 
 	@Override
-	public int getPosition() {
-		return this.pos;
-	}
+	public void update(Coordinate coordinate) {
 
-	public void UpDate(int NewPos) {
+		this.coordinate = coordinate;
+		this.viewPawn.setLocation(this.coordinate.getX(), this.coordinate.getY());
 		
 	}
 
