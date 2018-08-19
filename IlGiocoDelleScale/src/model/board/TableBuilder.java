5d059baca06238432453d33c994eb0644c5daca0
Amedeo Bertuccioli <amedeo.bertuccioli@studio.unibo.it>
2018-08-19 15:52:44 +0200

    Nothing change

--- IlGiocoDelleScale/src/model/board/TableBuilder.java
+++ IlGiocoDelleScale/src/model/board/TableBuilder.java
@@ -1,14 +1,7 @@
 package model.board;
 
-<<<<<<< HEAD
-import java.util.Random;
 
-
-import enumeration.MapDifficulty;
-import enumeration.MapDimension;
-=======
 import java.util.List;
->>>>>>> deb2e19cc7d59f2b1168bb7658d9b18f3519918e
 
 public interface TableBuilder {
 	
