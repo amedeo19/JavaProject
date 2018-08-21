2b9b1a5d6c4e1d4d9ad550a603c5411783fa8bd5
Thomas <thomas.angelini@studio.unibo.it>
2018-08-21 17:53:56 +0200

    Gui interface

---
+++ IlGiocoDelleScale/src/view/board2/GUI.java
@@ -0,0 +1,19 @@
+package view.board2;
+
+import controller.Controller;
+import javafx.fxml.Initializable;
+import view.view.ViewGuiImpl;
+
+public interface Gui extends Initializable{
+
+	public void SetText();
+	
+	public void RollDice();
+	
+	public void setView(ViewGuiImpl view);
+	
+	public void setController(Controller controller);
+	
+	
+	
+}
