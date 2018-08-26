d49b6b34aab2534163506fc7966f89ad6f2ac52a
Thomas <thomas.angelini@studio.unibo.it>
2018-08-26 21:03:16 +0200

    extends abstarct class

--- IlGiocoDelleScale/src/view/end/EndGuiBaghera.java
+++ IlGiocoDelleScale/src/view/end/EndGuiBaghera.java
@@ -1,46 +1,8 @@
 package view.end;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import view.view.View;
-import view.view.ViewGuiImpl;
 
-public class EndGuiBaghera implements EndGui {
-
-	@FXML
-	private Button exit;
-	
-	@FXML
-	private Button restart;
-	
-	
-	private View view;
-	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		this.exit.setVisible(true);
-		this.restart.setVisible(true);
-	}
+public class EndGuiBaghera extends AbstractEnd{
 
 	
-	@Override
-	public void exit() {
-		System.exit(0);
-	}
-
-
-	@Override
-	public void setView(ViewGuiImpl view) {
-		this.view = view;
-		
-	}
-
-
-	@Override
-	public void restart() {
-		this.view.restart();		
-	}
 	
 }
--- IlGiocoDelleScale/src/view/end/EndGuiBaloo.java
+++ IlGiocoDelleScale/src/view/end/EndGuiBaloo.java
@@ -1,43 +1,7 @@
 package view.end;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import view.view.View;
-import view.view.ViewGuiImpl;
 
-public class EndGuiBaloo implements EndGui {
+public class EndGuiBaloo extends AbstractEnd {
 
-	@FXML
-	private Button exit;
-	
-	@FXML
-	private Button restart;
-	
-	
-	private View view;
-	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		this.exit.setVisible(true);
-		this.restart.setVisible(true);
-		
-	}
-
-	@Override
-	public void exit() {
-		System.exit(0);
-	}
-	
-	@Override
-	public void restart() {
-		this.view.restart();
-	}
-	
-	@Override
-	public void setView(ViewGuiImpl view) {
-		this.view = view;
-	}
 	
 }
