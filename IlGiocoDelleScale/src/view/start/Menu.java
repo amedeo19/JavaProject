c6ac9643b39a2b9e7f3548434e74a4295bffb3b4
Luca Ghigi <ghigi96@hotmail.it>
2018-09-12 10:18:05 +0200

    added javadoc

--- IlGiocoDelleScale/src/view/start/Menu.java
+++ IlGiocoDelleScale/src/view/start/Menu.java
@@ -3,13 +3,41 @@ package view.start;
 import javafx.fxml.Initializable;
 import view.view.ViewGuiImpl;
 
+/**
+ * Interface for the view of the game.
+ */
 public interface Menu extends Initializable {
+	/**
+	 * set initial parameters and call pawn choices menu
+	 */
 	public void SelectPawns();
+	/**
+	 * close application
+	 */
 	public void Exit();
+	/**
+	 * method to set the right number of dices
+	 */
 	public void Update();
+	/**
+	 * method to confirm dice selection and enable multiface inputbox
+	 */
 	public void Update1();
+	/**
+	 * return to the initial menu
+	 */
 	public void Back();
+	/**
+	 * confirm character selection and pass to the dice selection menu
+	 */
 	public void SelectDice();
+	/**
+	 * initialize game
+	 */
 	public void StartGame();
+	/**
+	 * method to set the correct controller to the view
+	 * @param view the view parameter to pass
+	 */
 	public void setViewGuiImpl(ViewGuiImpl view);
 }
