0a7e1caebdcc4ae51473b287db707a8173325db2
Thomas <thomas.angelini@studio.unibo.it>
2018-09-13 14:39:19 +0200

    add javadoc

--- IlGiocoDelleScale/src/model/model/Model.java
+++ IlGiocoDelleScale/src/model/model/Model.java
@@ -4,7 +4,17 @@ import model.pawns.Pawns;
 
 public interface Model {
 
+	/**
+	 * Control if the player have win
+	 * @param p is the pawn
+	 * @return a flag that check win or not
+	 */
 	public boolean checkWin(Pawns p);
 	
+	/**
+	 * Move the pawn
+	 * @param p is the pawn
+	 * @return the new position of the pawn without a possible jump
+	 */
 	public int movePawn(Pawns p);
 }
